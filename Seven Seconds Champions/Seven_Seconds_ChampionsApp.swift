19bfbdb627396cac412baf11aa789b81a598aa92
Ovidiu Muntean <ovidiu.muntean@imawo.ro>
2025-01-09 15:21:24 +0200

    Update

--- Seven Seconds Champions/Seven_Seconds_ChampionsApp.swift
+++ Seven Seconds Champions/Seven_Seconds_ChampionsApp.swift
@@ -10,29 +10,9 @@ import SwiftData
 
 @main
 struct Seven_Seconds_ChampionsApp: App {
-<<<<<<< HEAD
-=======
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
->>>>>>> main
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-<<<<<<< HEAD
-=======
-        .modelContainer(sharedModelContainer)
->>>>>>> main
     }
 }
